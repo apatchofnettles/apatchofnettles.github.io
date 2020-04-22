b1a7db9dfb90cbdd235d5af51228d54a905c9d59
apatchofnettles <61041751+apatchofnettles@users.noreply.github.com>
2020-04-22 19:28:49 +1000

    Add files via upload

---
+++ nettles/main.js
@@ -0,0 +1,80 @@
+//////////////////////////////////////////////////////////////////
+//
+// Shuffle player launch point
+//
+//////////////////////////////////////////////////////////////////
+
+var SCrunning;
+
+// jQuery main()
+$(function () {
+	SCrunning = null;
+	SCrunning = new SCoperator();
+	SCrunning.configAPI("V2");
+	SCrunning.tryAutoPlay = function () {}
+
+
+	////////////////////////////////////////////////////////////////
+	// SoundCloud operator
+
+	SCrunning.getClient(function () {
+	
+	var allPages = 
+		[
+		"apatchofnettles",
+		"ascythethroughnettles",
+		"asoupofnettles",
+		"andretthenettles",
+		"tosowgreennettles",
+		"pangaeanpermafaction",
+		"user-512294964"
+		];
+		var triedLaunch = false;
+		for(var i=0; i < allPages.length; i++) {
+			if ( i == allPages.length-1 ) {
+				SCrunning.tryAutoPlay = function () {
+					var self = this;
+					setTimeout(function () {
+						if (self.mediaCheck(self)) {
+							self.actOnMedia(self, $(".sound-tile").length);
+						}
+					}, self.mediaDelay);
+				}
+			}
+			SCrunning.artist = allPages[i];
+			SCrunning.resolve("https://soundcloud.com/" + SCrunning.artist, function (self, resolved) {
+				self.artist_id = resolved.id;
+				self.artist_data = {
+					avatar: resolved.avatar_url
+				};
+				// get all artist 'TRACK' entries:
+				SCrunning.rackArtist(self.rackTracks);
+				// get optionally filtered playlists:
+				// SCrunning.rackCarts(self.rackTracks);
+			});
+		};
+	});
+
+
+	////////////////////////////////////////////////////////////////
+	// tiles
+
+	var base = document.querySelector('#TrackList'); // the container for the variable content
+	var selector = '.sound-tile'; // any css selector for children
+
+	base.addEventListener('click', function (event) {
+		// find the closest match of the event target
+		var closest = event.target.closest(selector);
+		if (closest && base.contains(closest) && SCrunning.isPlayable(closest)) {
+			SCrunning.spoolTrack(closest);
+		}
+	});
+
+	////////////////////////////////////////////////////////////////
+	// play bar
+
+	$("audio").on("ended", function () {
+		SCrunning.actOnMedia(SCrunning, $(".sound-tile").length);
+	});
+
+});
---
+++ salamandr/main.js
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////
+//
+// Shuffle player launch point
+//
+//////////////////////////////////////////////////////////////////
+
+var SCrunning;
+
+// jQuery main()
+$(function () {
+	SCrunning = null;
+	SCrunning = new SCoperator();
+	SCrunning.configAPI("V2");
+
+
+	////////////////////////////////////////////////////////////////
+	// SoundCloud operator
+
+	SCrunning.artist = "yellow-salamandr-4";
+	
+	SCrunning.matchPlaylists = 
+	[
+	"https://soundcloud.com/yellow-salamandr-4/sets/yellow-salamandr-entire",
+	"https://soundcloud.com/yellow-salamandr-4/sets/ys4"
+	];
+
+	SCrunning.getClient(function () {
+		SCrunning.resolve("https://soundcloud.com/" + SCrunning.artist, function (self, resolved) {
+			self.artist_id = resolved.id;
+			self.artist_data = {
+				avatar: resolved.avatar_url
+			};
+			// get all artist 'TRACK' entries:
+			SCrunning.rackArtist(self.rackTracks);
+			// get optionally filtered playlists:
+			SCrunning.rackCarts(self.rackTracks);
+		});
+	});
+
+
+	////////////////////////////////////////////////////////////////
+	// tiles
+
+	var base = document.querySelector('#TrackList'); // the container for the variable content
+	var selector = '.sound-tile'; // any css selector for children
+
+	base.addEventListener('click', function (event) {
+		// find the closest match of the event target
+		var closest = event.target.closest(selector);
+		if (closest && base.contains(closest) && SCrunning.isPlayable(closest)) {
+			SCrunning.spoolTrack(closest);
+		}
+	});
+
+	////////////////////////////////////////////////////////////////
+	// play bar
+
+	$("audio").on("ended", function () {
+		SCrunning.actOnMedia(SCrunning, $(".sound-tile").length);
+	});
+
+});
